00ee8422469b55257cdbadc7e347740110a3bed0
Jadson204 <jadsonhenrique2004@gmail.com>
2023-01-10 18:58:39 -0300

    10

---
+++ 10-scanf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int num1, num2, num3;
+
+  printf("Digite tres valores inteiros: ");
+  scanf("%d%d%d", &num1, &num2, &num3);
+
+  printf("Valores lidos: %d, %d, %d", num1, num2, num3);
+  return 0;
+}
