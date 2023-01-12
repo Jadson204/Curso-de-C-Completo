a005954292c0f51c84f916795b4c0dc27b07d575
Jadson204 <jadsonhenrique2004@gmail.com>
2023-01-12 07:04:10 -0300

    16

---
+++ 16-tamanho-dado.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+  float x = 1.0;
+
+  printf("Tamanho em memoria de um float: %d bytes\n", sizeof(x));
+
+  printf("Tamanho dem memoria de um int: %d bytes\n", sizeof(int));
+
+  return 0;
+}
