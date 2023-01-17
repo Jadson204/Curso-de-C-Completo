3bcde4259a5194d601ed2c723619355c5a63ce4d
Jadson204 <jadsonhenrique2004@gmail.com>
2023-01-17 08:06:48 -0300

    23 - O que são caracteres de escape \n, \t, \", \' e \\

---
+++ Aulas/23-caracteres-escape.c
@@ -0,0 +1,20 @@
+/* Aula 23 - O que são caracteres de escape \n, \t, \", \' e \\ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+  Caracteres de escape
+  Quebra de linha -> \n
+  Tabulação -> \t
+  Aspa dupla -> \"
+  Aspa simples -> \'
+  Contrabarra -> \\
+*/
+
+int main() {
+
+  printf("\n\n\tIsto e uma mensagem \'a\' \\ ser impressa na tela.\n\n");
+
+  return 0;
+}
