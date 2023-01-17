44af5c6ba2f47f20edb2d10aef5c5f767f236deb
Jadson204 <jadsonhenrique2004@gmail.com>
2023-01-17 09:00:41 -0300

    24 - Como usar as operações básicas da matemática na linguagem C

---
+++ Aulas/24-operadores-matematicos.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+   * Operadores matemáticos:
+   * Soma -> +
+   * Subtração -> -
+   * Multiplicação -> *
+   * Divisão -> /
+*/
+
+int main() {
+
+  float num1 = 10.0;
+  float num2 = 20.0;
+
+  printf("\nSoma: %.1f\n", num1 + num2);
+  printf("\nSubtracao: %.1f\n", num1 - num2);
+  printf("\nMultiplicacao: %.1f\n", num1 * num2);
+  printf("\nDivisao: %.1f\n", num1 / num2);
+}
