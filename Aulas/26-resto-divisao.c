2da50edace7244b04f05e10f7de75b23c1a41783
Jadson204 <jadsonhenrique2004@gmail.com>
2023-01-17 09:28:49 -0300

    26 - Como usar o operador de resto da divisão %

---
+++ Aulas/26-resto-divisao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  int num1 = 10;
+  int num2 = 20;
+
+  // Resto de 10 / 20 = 10
+
+  printf("Resto da divisao inteira: %d\n", num1 % num2);
+
+  return 0;
+}
