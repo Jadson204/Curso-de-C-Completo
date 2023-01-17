9be099c44793b2cccc311a31b5f272f025d1a48c
Jadson204 <jadsonhenrique2004@gmail.com>
2023-01-17 09:13:04 -0300

    25 - O que significa Casting ou conversão de tipos na linguagem C

---
+++ Aulas/25-casting.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+   * Operadores matemáticos:
+   * Soma -> +
+   * Subtração -> -
+   * Multiplicação -> *
+   * Divisão -> /
+*/
+
+int main() {
+
+  int num1 = 10;
+  int num2 = 20;
+
+  printf("\nSoma: %d\n", num1 + num2);
+  printf("\nSubtracao: %d\n", num1 - num2);
+  printf("\nMultiplicacao: %d\n", num1 * num2);
+  printf("\nDivisao: %f\n", (float)num1 / num2);
+}
