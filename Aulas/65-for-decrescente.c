65aafd8a341a93967c88291b699b75bb94f7c3d0
Jadson204 <jadsonhenrique2004@gmail.com>
2023-01-24 20:39:02 -0300

    65 - Estrutura de repetição for decrescente

---
+++ Aulas/65-for-decrescente.c
@@ -0,0 +1,14 @@
+// 65 - Estrutura de repetição for decrescente
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int i;
+
+  for (i = 10; i >= 0; i--) {
+    printf("%d\n", i);
+  }
+  
+  return 0;
+}
